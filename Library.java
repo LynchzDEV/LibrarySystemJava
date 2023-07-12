ece944b31bc425c69677c3713cc2b46f340400a6
Lynchz <100924535+LynchzDEV@users.noreply.github.com>
2023-07-12 22:00:37 +0700

    Update Library.java

--- Library.java
+++ Library.java
@@ -2,15 +2,17 @@ package librarymanagement;
 
 public class Library {
     private Book[] books;
-    private int capacity;
-    private int numBooks;
+    private int capacity;  
+    private int numBooks;  // Number of books currently in the library
 
+    // Constructor
     public Library(int capacity) {
         books = new Book[capacity];
         this.capacity = capacity;
-        numBooks = 0;
+        numBooks = 0; 
     }
 
+    // Add a book to the library
     public void addBook(Book book) {
         if (numBooks < capacity) {
             books[numBooks] = book;
@@ -21,6 +23,7 @@ public class Library {
         }
     }
 
+    // Display all available books
     public void displayAvailableBooks() {
         System.out.println("Available Books:");
         for (int i = 0; i < numBooks; i++) {
@@ -31,6 +34,7 @@ public class Library {
         }
     }
 
+    // Borrow a book
     public void borrowBook(String title) {
         Book book = findBookByTitle(title);
         if (book != null) {
@@ -45,6 +49,7 @@ public class Library {
         }
     }
 
+    // Return a book
     public void returnBook(String title) {
         Book book = findBookByTitle(title);
         if (book != null) {
@@ -59,10 +64,11 @@ public class Library {
         }
     }
 
+    // Helper method to find a book by title
     private Book findBookByTitle(String title) {
         for (int i = 0; i < numBooks; i++) {
             Book book = books[i];
-            if (book.getTitle().equalsIgnoreCase(title)) {
+            if (book.getTitle().equalsIgnoreCase(title)) { // .equalsIgnoreCase() is used to compare strings ignoring case 
                 return book;
             }
         }
