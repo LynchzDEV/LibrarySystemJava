f1d138cd8a83f69d361fca4cc2a42d49ba7651ef
Lynchz <100924535+LynchzDEV@users.noreply.github.com>
2023-07-12 17:26:19 +0700

    Update Library.java

--- Library.java
+++ Library.java
@@ -7,13 +7,11 @@ public class Library {
     private List<Book> books;
     private int capacity;
 
-    // Constructor
     public Library(int capacity) {
         books = new ArrayList<>();
         this.capacity = capacity;
     }
 
-    // Add a book to the library
     public void addBook(Book book) {
         if (books.size() < capacity) {
             books.add(book);
@@ -23,50 +21,49 @@ public class Library {
         }
     }
 
-    // Display all available books
     public void displayAvailableBooks() {
         System.out.println("Available Books:");
-        for (int i = 0; i < books.size(); i++) {
-            Book book = books.get(i);
+        for (Book book : books) {
             if (book.isAvailable()) {
-                System.out.println("Title: " + book.getTitle() + ", Author: " + book.getAuthor());
+                System.out.println(book);
             }
         }
     }
 
-    // Borrow a book
     public void borrowBook(String title) {
-        for (int i = 0; i < books.size(); i++) {
-            Book book = books.get(i);
-            if (book.getTitle().equalsIgnoreCase(title)) {
-                if (book.isAvailable()) {
-                    book.setAvailable(false);
-                    System.out.println("You have successfully borrowed the book: " + book.getTitle());
-                    return;
-                } else {
-                    System.out.println("Sorry, the book is currently unavailable.");
-                    return;
-                }
+        Book book = findBookByTitle(title);
+        if (book != null) {
+            if (book.isAvailable()) {
+                book.setAvailable(false);
+                System.out.println("You have successfully borrowed the book: " + book.getTitle());
+            } else {
+                System.out.println("Sorry, the book is currently unavailable.");
             }
+        } else {
+            System.out.println("Book not found in the library.");
         }
-        System.out.println("Book not found in the library.");
     }
 
-    // Return a book
     public void returnBook(String title) {
-        for (int i = 0; i < books.size(); i++) {
-            Book book = books.get(i);
+        Book book = findBookByTitle(title);
+        if (book != null) {
+            if (!book.isAvailable()) {
+                book.setAvailable(true);
+                System.out.println("You have successfully returned the book: " + book.getTitle());
+            } else {
+                System.out.println("You have already returned this book.");
+            }
+        } else {
+            System.out.println("Book not found in the library.");
+        }
+    }
+
+    private Book findBookByTitle(String title) {
+        for (Book book : books) {
             if (book.getTitle().equalsIgnoreCase(title)) {
-                if (!book.isAvailable()) {
-                    book.setAvailable(true);
-                    System.out.println("You have successfully returned the book: " + book.getTitle());
-                    return;
-                } else {
-                    System.out.println("You have already returned this book.");
-                    return;
-                }
+                return book;
             }
         }
-        System.out.println("Book not found in the library.");
+        return null;
     }
 }
